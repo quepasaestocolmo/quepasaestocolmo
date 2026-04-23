6f35fab5dcc4199032f5f5a1a60a443f6b77a5d3
quepasaestocolmo <quepasaestocolmo@gmail.com>
2026-04-23 18:56:10 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,24 @@
+export default function sitemap() {
+  const baseUrl = 'https://www.quepasaestocolmo.com'
 
+  return [
+    {
+      url: `${baseUrl}/cerezos-estocolmo-2026`,
+      lastModified: '2026-04-01',
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/blog/que-hacer-estocolmo-primavera-2026`,
+      lastModified: '2026-04-01',
+      changeFrequency: 'monthly',
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/en/blog/what-to-do-stockholm-spring-2026`,
+      lastModified: '2026-04-01',
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+  ]
+}
